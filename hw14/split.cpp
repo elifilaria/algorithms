dd1fd3eabfe7313ab54859a1ef54badfcf2e2c84
Elif I Yurtseven <yurtse1@stolaf.edu>
2017-03-10 03:29:45 -0600

    hw14 questions 7 and 8 completed respectively

---
+++ hw14/split.cpp
@@ -0,0 +1,115 @@
+#include<vector>
+#include<iostream>
+#include<fstream>
+#include <cstdlib>
+using namespace std;
+ 
+vector<string> split(const string &line, const char *delim = " \t")
+{
+  vector<string> word;
+  size_t loc0, loc1;
+  loc0 = line.find_first_not_of(delim);
+  if ( loc0 == string::npos ) return word;
+  loc1 = line.find_first_of(delim, loc0);
+  while ( loc1 != string::npos ) {
+    word.push_back( line.substr(loc0, loc1-loc0) );
+    loc0 = line.find_first_not_of(delim, loc1);
+    if ( loc0 == string::npos ) break;
+    loc1 = line.find_first_of(delim, loc0);
+  }
+  if ( loc0 != string::npos && loc1 == string::npos )
+    word.push_back( line.substr(loc0, line.size()-loc0) );
+  return word;
+}
+
+
+struct AdjListNode
+{
+    int dest;
+    struct AdjListNode* next;
+};
+ 
+
+
+  
+struct AdjList
+{
+    struct AdjListNode *head;
+};
+ 
+/*
+ * Class Graph
+ */ 
+class Graph
+{
+    private:
+        int V;
+        struct AdjList* array;
+    public:
+        Graph(int V)
+        {
+            this->V = V;
+            array = new AdjList [V];
+            for (int i = 0; i < V; ++i)
+                array[i].head = NULL;
+        }
+        /*
+         * Creating New Adjacency List Node
+         */ 
+        AdjListNode* newAdjListNode(int dest)
+        {
+            AdjListNode* newNode = new AdjListNode;
+            newNode->dest = dest;
+            newNode->next = NULL;
+            return newNode;
+        }
+        /*
+         * Adding Edge to Graph
+         */ 
+        void addEdge(int src, int dest)
+        {
+            AdjListNode* newNode = newAdjListNode(dest);
+            newNode->next = array[src].head;
+            array[src].head = newNode;
+            newNode = newAdjListNode(src);
+            newNode->next = array[dest].head;
+            array[dest].head = newNode;
+        }
+        /*
+         * Print the graph
+         */ 
+        void printGraph()
+        {
+            int v;
+            for (v = 0; v < V; ++v)
+            {
+                AdjListNode* pCrawl = array[v].head;
+                cout<<"\n Adjacency list of vertex "<<v<<"\n head ";
+                while (pCrawl)
+                {
+                    cout<<"-> "<<pCrawl->dest;
+                    pCrawl = pCrawl->next;
+                }
+                cout<<endl;
+            }
+        }
+};
+
+
+
+ 
+int main()
+{
+  ifstream f("file.txt");
+  string line;
+  Graph a(5);
+  while ( getline(f, line) ) {
+    // split the line into words
+    vector<string> word = split(line);
+  if (word[0] == "e" ) {
+       a.addEdge(stoi(word[1]), stoi(word[2]));
+}}
+  f.close();
+  a.printGraph();
+}
+
